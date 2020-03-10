25442bb93e4ca6d2dc0f761e83fb40ba2e36493f
KevCasHSC <castrok.c11@hotmail.com>
2020-03-10 17:02:41 +0000

    last upload

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+/**
+ * binary tree node - function to create a binary tree node
+ * @parent: pointer to the parent node
+ * @value: value to put in the new node
+ * Return: the pointer to the new node
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node;
+
+	node = malloc(sizeof(binary_tree_t));
+	if (!node)
+	{
+		return (NULL);
+	}
+
+	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
+	node->parent = parent;
+	return (node);
+}
