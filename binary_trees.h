cbb3d73a30909c99c30528085632d481c2f60928
KevCasHSC <castrok.c11@hotmail.com>
2020-03-10 16:09:02 +0000

    last upload

--- binary_trees.h
+++ binary_trees.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <stddef.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -21,6 +22,8 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+/*function to print binary trees*/
+void binary_tree_print(const binary_tree_t *);
 /*function that create a binary tree node*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 #endif
