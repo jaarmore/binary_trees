036de98086e0ccfa1f3d2faf6b10c28523d05ae5
KevCasHSC <castrok.c11@hotmail.com>
2020-03-10 21:14:42 +0000

    fixing betty style

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 /**
- * binary_tree_delete - function that deletes an entire binary tree
+ * binary_tree_postorder - function that deletes an entire binary tree
  * @tree: pointer to root of the tree.
+ * @func: pointer to the function to print
  * Return: nothing.
 */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
