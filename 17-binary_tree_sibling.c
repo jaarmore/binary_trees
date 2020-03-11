12e62ae2ec97d90a4847d043b8fca03b656aba5d
jaarmore <jaarmore@gmail.com>
2020-03-11 16:35:26 -0500

    check if parent->node is different of NULL

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -12,8 +12,10 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (node->parent == NULL)
 		return (NULL);
 
-	if (node->parent->left->n == node->n)
+	if (node->parent->left->n == node->n && node->parent->right != NULL)
 		return (node->parent->right);
-	else
+	else if (node->parent->right->n == node->n && node->parent->left != NULL)
 		return (node->parent->left);
+	else
+		return (NULL);
 }
