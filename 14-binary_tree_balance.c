b54ae5e801dd07aa89b640eefb313a1d1cf770f2
KevCasHSC <castrok.c11@hotmail.com>
2020-03-11 19:17:28 +0000

    last upload

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,7 +7,7 @@
  */
 size_t max(size_t a, size_t b)
 {
-        return ((a > b) ? a : b);
+	return ((a > b) ? a : b);
 }
 /**
  * binary_tree_height - function that measures the height of a binary tree.
@@ -16,19 +16,19 @@ size_t max(size_t a, size_t b)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t left = 0;
-        size_t right = 0;
+	size_t left = 0;
+	size_t right = 0;
 
-        if (!tree)
-                return (0);
+	if (!tree)
+		return (0);
 
-        if (tree->left)
-                left = 1 + binary_tree_height(tree->left);
+	if (tree->left)
+		left = 1 + binary_tree_height(tree->left);
 
-        if (tree->right)
-                right = 1 + binary_tree_height(tree->right);
+	if (tree->right)
+		right = 1 + binary_tree_height(tree->right);
 
-        return (max(left, right));
+	return (max(left, right));
 }
 /**
  * binary_tree_balance - function to calculate the balance factor of a Btree
