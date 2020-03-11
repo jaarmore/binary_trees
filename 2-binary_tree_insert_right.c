1f95491cc0b66983337837bff1e648eeb5f1018f
jaarmore <jaarmore@gmail.com>
2020-03-11 13:08:45 -0500

    check malloc on file

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -15,6 +15,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 
 	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
 	node->left = NULL, node->right = NULL;
 	node->n = value;
 
