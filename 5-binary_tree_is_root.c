fd777e286e3a873205c8e56842350fef12e6e3df
KevCasHSC <castrok.c11@hotmail.com>
2020-03-11 18:20:46 +0000

    last upload

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -9,14 +9,12 @@ int binary_tree_is_root(const binary_tree_t *node)
 	if (node == NULL)
 		return (0);
 
-	if (node->left != NULL && node->right != NULL)
+	if (node->parent == NULL)
 	{
 		return (1);
 	}
 	else
 	{
-		binary_tree_is_root(node->left);
-		binary_tree_is_root(node->right);
+		return (0);
 	}
-	return (0);
 }
